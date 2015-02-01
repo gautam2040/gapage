3482b3da68698a4a993ef3324e319cbaef11dcae
maha <maharshi849@gmail.com>
2015-02-01 19:34:32 +0530

    testimonial jquery

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -3,6 +3,8 @@ $( document ).ready(function() {
   smoothScroll(1000);
   workbelt();
   workLoad();
+  clientStuff();
+
 
 });
 
@@ -51,5 +53,69 @@ function workLoad() {
 		$('.project-load').html(spinner).load(newHTML); 
 
 	});
+
+}
+
+
+
+
+
+function clientStuff(){
+
+	$('.client-unit').first().addClass('active-client');
+	$('.client-logos img').first().addClass('active-client');
+
+	$('.client-logos img').click(function(){
+		// alert("hito hito");
+		var $allimg = $(this).parent().children();
+		var position = $allimg.index($(this));
+		$('.client-logos img').removeClass('active-client');
+		$(this).addClass('active-client');
+		$('.client-unit').removeClass('active-client');
+		$('.client-unit').eq(position).addClass('active-client');
+
+	});
+
+
+
+	$('.rarrow').click(function(){
+
+			 var thisUnit = $('.client-unit.active-client').index();
+			 var total = $('.client-unit').parent().children().length;
+
+			 console.log(thisUnit);
+			
+			if( thisUnit == total-1)
+			{
+				$('.client-unit').removeClass('active-client').first().addClass('active-client');
+				$('.client-logos img').removeClass('active-client').first().addClass('active-client');
+
+			}
+			else{		
+				$('.client-unit.active-client').removeClass('active-client').next().addClass('active-client');
+				$('.client-logos img.active-client').removeClass('active-client').next().addClass('active-client');
+
+			}
+		});
+
+		$('.larrow').click(function(){
+
+			var thisUnit = $('.client-unit.active-client').index();
+			var total = $('.client-unit').parent().children().length;
+
+
+			
+			if( thisUnit == 0)
+			{
+				$('.client-unit').removeClass('active-client').last().addClass('active-client');
+				$('.client-logos img').removeClass('active-client').last().addClass('active-client');
+
+			}
+			else{		
+				$('.client-unit.active-client').removeClass('active-client').prev().addClass('active-client');
+				$('.client-logos img.active-client').removeClass('active-client').prev().addClass('active-client');
+
+			}
+		});
 
 }
